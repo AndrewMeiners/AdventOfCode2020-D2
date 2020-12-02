cbd9f2a37ba0bc9939e29db8f0fb90df8af6f94b
Andrew Meiners <Andrew.Meiners@Gmail.com>
2020-12-02 13:48:46 -0500

    D2 P2 complete

--- D2/Program.cs
+++ D2/Program.cs
@@ -16,19 +16,28 @@ namespace D2
                 // delims = ' ', '-', ':'
                 string[] parts = line.Split(delimiterChars);
 
+                /* P1
                 int min = Convert.ToInt32(parts[0]);
                 int max = Convert.ToInt32(parts[1]);
                 char searchChar = Convert.ToChar(parts[2]);
                 string passwordString = parts[4];
 
-                //Console.WriteLine(min);
-                //Console.WriteLine(max);
-                //Console.WriteLine(searchChar);
-                //Console.WriteLine(passwordString);
-
+                
                 int charOccuranceCount = passwordString.Count(s => s == searchChar);
 
                 if (min <= charOccuranceCount && charOccuranceCount <= max) validPasswordCount++;
+                */
+
+                // P2
+                int index1 = Convert.ToInt32(parts[0]) - 1;
+                int index2 = Convert.ToInt32(parts[1]) - 1;
+                char searchChar = Convert.ToChar(parts[2]);
+                string passwordString = parts[4];
+
+                if ((passwordString[index1] == searchChar && passwordString[index2] != searchChar) || (passwordString[index2] == searchChar && passwordString[index1] != searchChar))
+                {
+                    validPasswordCount++;
+                }
             }
             Console.WriteLine(validPasswordCount);
         }
